0708932ae99db9510a9b86fe1303e36fb1553254
Thomas-Grassi <grassi.thomas@itiscannizzaro.edu.it>
2025-04-01 09:36:47 +0200

    Create Settings.java

---
+++ app/src/main/java/com/example/applicazionelogin/Settings.java
@@ -0,0 +1,42 @@
+package com.example.applogin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_settings);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void okButtonClicked(View view) {
+        Switch darkModeSwitch = findViewById(R.id.darkModeSwitch);
+        if(darkModeSwitch.isChecked()){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+    }
+
+    public void goBackHomePageButtonClicked(View view) {
+        Intent intent = new Intent(this, HomePage.class);
+        startActivity(intent);
+    }
+}
